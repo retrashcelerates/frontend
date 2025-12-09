9ea6d646e715e12248421224cb8ae0eea31f93ae
danvhl <holdaninurrafi026@gmail.com>
2025-12-09 16:17:59 +0700

    penambahan dashboard

---
+++ src/components/Footeradmin.jsx
@@ -0,0 +1,11 @@
+export default function Footeradmin() {
+  return (
+    <footer className="bg-[#3DBD59] text-white p-10 sm:px-20 lg:px-32 w-full">
+      <div className="w-full flex justify-center">
+        <p className="text-sm text-center opacity-90">
+          Copyright © 2025. ReTrash. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
